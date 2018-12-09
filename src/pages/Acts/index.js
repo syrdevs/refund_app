b5ded4ed8a65955122b6f6dbfb55a296c6e9f02a
syrdevs <syrdevs@gmail.com>
2018-12-10 03:49:44 +0600

    Исправление по роутингам

--- src/pages/Acts/index.js
+++ src/pages/Acts/index.js
@@ -7,20 +7,12 @@ import { formatMessage, FormattedMessage } from 'umi/locale';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
 
-const dateFormat = 'DD.MM.YYYY';
 export default class Acts extends Component {
-  state = {
-  };
 
 
 
   render = () => {
     const { location, children } = this.props;
-    return (<PageHeaderWrapper title={formatMessage({ id: 'app.module.acts.title' })}>
-        <Card bodyStyle={{ padding: 5 }}>
-          {children}
-        </Card>
-      </PageHeaderWrapper>
-    );
+    return children;
   };
 }
